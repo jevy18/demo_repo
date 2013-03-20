d363d202befc5b86fd7847927639d4bf1211fdef
jevy <jevy18@hotmail.com>
2013-03-20 13:15:44 -0500

    testing the demo.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 	
-	/**
-	 * @param args
-	 */
-	
 	public static void main(String[] args) {
+		
+		
 		int count = 0;
 		while(count < 4) {
 			System.out.println("val:" + ++count);
 		}
 		
+		
+		System.out.println("Display contents");
 		System.out.println("Hello World");
 		System.out.println("Testing this git coding sytle");
 		System.out.println("business updates");
@@ -21,6 +23,30 @@ public class Main {
 
         System.arraycopy(copyFrom, 2, copyTo, 0, 7);
         System.out.println(new String(copyTo));
-		
-	}
-}
+        
+        
+        Scanner scan = new Scanner(System.in);
+        int input = 0;
+        ArrayList <Integer> numbList = new ArrayList<Integer>(4);
+        for (int i=0; i<4; i++) {
+        	System.out.println("Enter a number to store: " );
+        	input = scan.nextInt();
+        	numbList.add(i, input);
+        	System.out.println("*******TESTING*******");
+        	System.out.println(numbList.toString());
+        }
+        
+        int counta = 0;
+        for (int index : numbList) {
+        	if(index > 10) {
+        		System.out.println("FETCHING ARRAYLIST VALUES");
+        		System.out.println("These values are greather than ten");
+        		System.out.println("greather value retrieved: " + numbList.get(counta));
+        	}else {
+        		System.out.println("These values are lesser than ten");
+        		System.out.println("lesser value retrieved: " + numbList.get(counta));
+        	}
+        	counta++;
+        }			
+	} // end of main method.
+}// end of Main class.
